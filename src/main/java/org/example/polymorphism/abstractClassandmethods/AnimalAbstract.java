e9665d2de56737455666f02f909ec3ff2db9c2da
ceobaig2023 <ceobaig2023@gmail.com>
2024-09-18 10:59:27 +0530

    Polymorphism abstract extends implements interface Inheritance @Overriding

---
+++ src/main/java/org/example/polymorphism/abstractClassandmethods/AnimalAbstract.java
@@ -0,0 +1,8 @@
+package org.example.polymorphism.abstractClassandmethods;
+
+abstract public class AnimalAbstract {
+    abstract public void sound();
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+}
