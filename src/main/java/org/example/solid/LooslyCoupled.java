15a8ea5fb66e11af38c438c78fc1b9d93390c14a
ceobaig2023 <ceobaig2023@gmail.com>
2024-11-06 14:30:58 +0530

    Tiglhtly  Couppled

--- src/main/java/org/example/solid/LooslyCoupled.java
+++ src/main/java/org/example/solid/LooslyCoupled.java
@@ -1,30 +1,47 @@
 package org.example.solid;
 
-class Engin{
-    void start(){
-        System.out.println("Engin start");
+interface Engine{
+
+    void start();
+}
+
+class PetrolEngine implements Engine{
+    @Override
+    public void start() {
+        System.out.println("Petrool Engine started");
     }
 }
-class Car{
-    private Engin e=new Engin();
 
-    void start(){
-        e.start();
-        System.out.println("Car start");
+class DieselEngine implements Engine{
+    @Override
+    public void start() {
+        System.out.println("Diesel Engine started");
     }
+}
+class Car1{
+    private Engine engine;
+
+    public Car1(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void startcar() {
+        engine.start();
+        System.out.println("Car started");
+    }
+
+
+
 }
 public class LooslyCoupled {
     public static void main(String[] args) {
 
-        Car c=new Car();
-        c.start();
+        Engine petrol=new PetrolEngine();
+        Car1 c=new Car1(petrol);
+        c.startcar();
 
+        Engine desil=new DieselEngine();
+        Car1 c1=new Car1(desil);
+        c1.startcar();
     }
 }
-
-/*"In Java, tight coupling means that classes are highly dependent on each other. For example, if I create an instance of Engine directly inside a Car class, the Car class becomes tightly coupled with Engine. This setup is rigid because if the Engine changes, we may need to change Car too.
-
-Loose coupling is achieved by reducing these dependencies, often using interfaces and dependency injection. Instead of Car creating Engine, we pass Engine into Car through a constructor. This way, Car only depends on the Engine interface, not any specific implementation. This makes the code flexible and allows us to easily switch between different Engine types, like DieselEngine or PetrolEngine, without modifying the Car class.
-
-In my experience, loose coupling is a good practice because it keeps code flexible and easier to test or extend. I’d typically aim to use loose coupling in projects to make maintenance and future changes simpler."
-*/
